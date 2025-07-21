6175f72436adc0fb1c5a1ce7e5a45843718de120
tincho <mvigil@fi.uba.ar>
2025-07-21 14:41:36 -0300

    agrego ruta peliculas a api.js

--- backend/api.js
+++ backend/api.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const usuariosRouter = require('./rutas/usuarios');
+const peliculasRouter = require('./rutas/peliculas');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use('/api/usuarios', usuariosRouter);
+app.use('/api/peliculas', peliculasRouter);
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
