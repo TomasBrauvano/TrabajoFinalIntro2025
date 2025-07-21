eaf088c729c57522af652ef09db1db7c0874683d
LeonardoCh200424 <lchaparro@fi.uba.ar>
2025-07-21 18:13:00 -0300

    Metodos get de la entidad Serie

--- backend/api.js
+++ backend/api.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const usuariosRouter = require('./rutas/usuarios');
+const seriesRouter = require('./rutas/series');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use('/api/usuarios', usuariosRouter);
+app.use('/api/series', seriesRouter);
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
