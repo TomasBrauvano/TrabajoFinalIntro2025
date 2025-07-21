fcfeb75d878feff9d1e5554a55e8b5f61ff22ca7
tincho <mvigil@fi.uba.ar>
2025-07-21 18:07:26 -0300

    agrega logica para obtener por creador y modifica funciones por cambios en la db

--- backend/modelos/pelicula.js
+++ backend/modelos/pelicula.js
@@ -10,27 +10,32 @@ async function obtenerPorCategoria(id_categoria) {
     return res.rows;
 }
 
+async function obtenerPorCreador(creador_id) {
+    const res = await pool.query('SELECT * FROM peliculas WHERE creador_id = $1', [creador_id]);
+    return res.rows;
+}
+
 async function obtenerTodas() {
     const res = await pool.query('SELECT * FROM peliculas');
     return res.rows;
 }
 
-async function crear({ nombre, anio, director, sinopsis, imagen, categoria }) {
+async function crear(creador_id, { nombre, anio, director, sinopsis, imagen, categoria }) {
     try {
         await pool.query(
-            'INSERT INTO peliculas (nombre, anio, director, sinopsis, imagen, categoria) VALUES ($1, $2, $3, $4, $5, $6)',
-            [nombre, anio, director, sinopsis, imagen, categoria]
+            'INSERT INTO peliculas (nombre, anio, director, sinopsis, imagen, creador_id, categoria) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+            [nombre, anio, director, sinopsis, imagen, creador_id, categoria]
         );
     } catch (err) {
         throw err;
     }
 }
 
-async function actualizar(id, { nombre, anio, director, sinopsis, imagen, categoria }) {
+async function actualizar(id, { nombre, anio, director, sinopsis, imagen, creador_id, categoria }) {
     try {
         const resultado = await pool.query(
-            'UPDATE peliculas SET nombre = $1 , anio = $2 , director = $3 , sinopsis = $4 , imagen = $5 , categoria = $6  WHERE id = $7 RETURNING *',
-            [nombre, anio, director, sinopsis, imagen, categoria, id]
+            'UPDATE peliculas SET nombre = $1 , anio = $2 , director = $3 , sinopsis = $4 , imagen = $5 , creador_id = $6 , categoria = $7  WHERE id = $8 RETURNING *',
+            [nombre, anio, director, sinopsis, imagen, creador_id, categoria, id]
         );
         return resultado.rows[0];
     } catch (err) {
@@ -49,4 +54,4 @@ async function eliminarPorId(id) {
     }
 }
 
-module.exports = { obtenerPorId, obtenerPorCategoria, obtenerTodas, crear, actualizar, eliminarPorId };
+module.exports = { obtenerPorId, obtenerPorCategoria, obtenerTodas, crear, actualizar, eliminarPorId, obtenerPorCreador };
