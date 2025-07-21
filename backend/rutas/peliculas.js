49330abb36bfea9b686cbc5f25a909cd660d0aee
tincho <mvigil@fi.uba.ar>
2025-07-21 15:39:10 -0300

    agrega endpoint para crear peliculas

--- backend/rutas/peliculas.js
+++ backend/rutas/peliculas.js
@@ -33,4 +33,21 @@ router.get("/categorias/:id_categoria", async (req, res) => {
         res.status(500).json({ error: 'Error en el servidor' });
     }
 })
+
+router.post("/", async (req, res) => {
+    const { nombre, anio, director, sinopsis, imagen, categoria } = req.body;
+
+    if (!nombre || !anio || !director || !sinopsis || !imagen || !categoria) {
+        return res.status(400).json({ error: 'Faltan campos' });
+    }
+
+    try {
+        await peliculaModelo.crear({ nombre, anio, director, sinopsis, imagen, categoria });
+        res.status(201).json({ mensaje: 'Pelicula creada' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error en el servidor' });
+    }
+})
+
 module.exports = router
