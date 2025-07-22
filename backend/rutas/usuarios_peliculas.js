3eda73a57af0cd8d3833e0dc66b966fb19875a5b
tincho <mvigil@fi.uba.ar>
2025-07-22 20:28:56 -0300

    agrega endpoint para actualizar peliculas agregadas por el usuario

--- backend/rutas/usuarios_peliculas.js
+++ backend/rutas/usuarios_peliculas.js
@@ -33,4 +33,23 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.put('/', async (req, res) => {
+    const { usuario_id, pelicula_id, calificacion, estado } = req.body;
+
+    if (!usuario_id || !pelicula_id || !estado) {
+        return res.status(400).json({ error: 'Faltan campos' });
+    }
+
+    try {
+        const relacionActualizada = await usuarioPeliculaModelo.actualizar(usuario_id, pelicula_id, calificacion, estado);
+        if (!relacionActualizada) {
+            return res.status(404).json({ error: 'La relacion no existe' });
+        }
+        res.status(201).json({ message: 'Pelicula actualizada en el perfil del usuario' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Error en el servidor' });
+    }
+})
+
 module.exports = router;
