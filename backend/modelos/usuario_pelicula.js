1cc1027c0c7b63b64a0392a66c9e920b1a00a287
tincho <mvigil@fi.uba.ar>
2025-07-22 20:04:09 -0300

    agrega logica para agregar peliculas

--- backend/modelos/usuario_pelicula.js
+++ backend/modelos/usuario_pelicula.js
@@ -7,4 +7,11 @@ async function obtenerPeliculasPorIdUsuario(usuario_id) {
     return res.rows;
 }
 
-module.exports = { obtenerPeliculasPorIdUsuario };
+async function agregar(usuario_id, pelicula_id, calificacion, estado) {
+    const res = await pool.query(
+        'INSERT INTO usuario_pelicula (usuario_id, pelicula_id, calificacion, estado) VALUES ($1,$2,$3,$4) RETURNING *',
+        [usuario_id, pelicula_id, calificacion, estado]);
+    return res.rows[0];
+}
+
+module.exports = { obtenerPeliculasPorIdUsuario, agregar };
