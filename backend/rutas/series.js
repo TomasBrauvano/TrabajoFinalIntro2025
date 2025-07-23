07394b8e715c25e45a94faabcc5610beb42f0a0d
tincho <mvigil@fi.uba.ar>
2025-07-23 16:59:54 -0300

    se crea relacion al crear una serie

--- backend/rutas/series.js
+++ backend/rutas/series.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const serieModelo = require('../modelos/serie');
+const usuarioSerieModelo = require('../modelos/usuarioSerie');
 
 //GET
-router.get("/", async (req, res) =>{
+router.get("/", async (req, res) => {
     try {
         const series = await serieModelo.obtenerTodas();
         res.status(200).json(series);
-    } catch (err){
+    } catch (err) {
         console.error(err);
-        res.status(500).json({error: 'Error en el servidor'});
+        res.status(500).json({ error: 'Error en el servidor' });
     }
 });
 
@@ -20,49 +21,50 @@ router.get("/creador/:creador_id", async (req, res) => {
         res.status(200).json(series);
     } catch (err) {
         console.error(err);
-        res.status(500).json({ error: 'Error en el servidor al obtener series por creador.'});
+        res.status(500).json({ error: 'Error en el servidor al obtener series por creador.' });
     }
 });
 
 
 router.get("/categoria/:id_categoria", async (req, res) => {
     const { id_categoria } = req.params;
-    try{
+    try {
         const series = await serieModelo.obtenerTodasPorCategoria(id_categoria);
         res.status(200).json(series);
-    } catch (err){
+    } catch (err) {
         console.error(err);
-        res.status(500).json({error: 'Error en el servidor'});
+        res.status(500).json({ error: 'Error en el servidor' });
     }
 });
 
-router.get("/:id", async (req, res) =>{
+router.get("/:id", async (req, res) => {
     const { id } = req.params;
-    try{
+    try {
         const serie = await serieModelo.obtenerPorId(id);
-        if(!serie){
-            return res.status(404).json({error: 'No se encontro la serie'});
+        if (!serie) {
+            return res.status(404).json({ error: 'No se encontro la serie' });
         }
         res.status(200).json(serie);
-    } catch (err){
+    } catch (err) {
         console.error(err);
-        res.status(500).json({error:'Error en el servidor'});
+        res.status(500).json({ error: 'Error en el servidor' });
     }
 });
 
 
 //POST
-router.post("/", async(req, res) => {
-    const {nombre, anio, director, sinopsis, imagen, creador_id, categoria} = req.body;
+router.post("/", async (req, res) => {
+    const { nombre, anio, director, sinopsis, imagen, creador_id, categoria, calificacion, estado } = req.body;
 
-    if(!nombre || !anio || !director || !sinopsis || !imagen || !creador_id || !categoria){
-        return res.status(400).json({error: 'Faltan campos'});
+    if (!nombre || !anio || !director || !sinopsis || !imagen || !creador_id || !categoria || !estado) {
+        return res.status(400).json({ error: 'Faltan campos' });
     }
 
-    try{
-        const nuevaSerie = await serieModelo.crear(creador_id,{nombre, anio, director, sinopsis, imagen, categoria});
+    try {
+        const nuevaSerie = await serieModelo.crear(creador_id, { nombre, anio, director, sinopsis, imagen, categoria });
+        await usuarioSerieModelo.crear(creador_id, nuevaSerie.id, calificacion, estado);
         res.status(201).json(nuevaSerie);
-    } catch (err){
+    } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Error en el servidor al crear la serie.' })
     }
@@ -70,37 +72,37 @@ router.post("/", async(req, res) => {
 
 //PUT
 router.put("/:id", async (req, res) => {
-    const {id} = req.params;
-    const {nombre, anio, director, sinopsis, imagen, creador_id, categoria, usuario_id} = req.body;
+    const { id } = req.params;
+    const { nombre, anio, director, sinopsis, imagen, creador_id, categoria, usuario_id } = req.body;
 
     if (!nombre || !anio || !director || !sinopsis || !imagen || !creador_id || !categoria || !usuario_id) {
         return res.status(400).json({ error: 'Faltan campos' });
     }
 
-    try{
+    try {
         const serieExistente = await serieModelo.obtenerPorId(id);
-        if(!serieExistente){
-            return res.status(404).json({ error: 'Serie no encontrada'});
+        if (!serieExistente) {
+            return res.status(404).json({ error: 'Serie no encontrada' });
         }
         if (serieExistente.creador_id != usuario_id) {
             return res.status(403).json({ error: 'No tienes permisos para actualizar esta serie.' });
         }
 
-        const serieActualizada = await serieModelo.actualizar(id,{nombre, anio, director, sinopsis, imagen, creador_id:serieExistente.creador_id, categoria});
+        const serieActualizada = await serieModelo.actualizar(id, { nombre, anio, director, sinopsis, imagen, creador_id: serieExistente.creador_id, categoria });
         res.status(200).json(serieActualizada);
 
-    }catch (err){
+    } catch (err) {
         console.error(err);
-        res.status(500).json({ error: 'Error en el servidor al actualizar la serie.'});
+        res.status(500).json({ error: 'Error en el servidor al actualizar la serie.' });
     }
 });
 
 //DELETE
 router.delete("/:id", async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const usuario_id = req.body.usuario_id;
 
-    try{
+    try {
         const serieExistente = await serieModelo.obtenerPorId(id);
 
         if (!serieExistente) {
@@ -113,9 +115,9 @@ router.delete("/:id", async (req, res) => {
 
         await serieModelo.eliminarPorId(id);
         res.status(200).json({ mensaje: 'Serie eliminada' });
-    } catch(err){
+    } catch (err) {
         console.error(err);
-        res.status(500).json({error: 'Error en el servidor al eliminar serie'})
+        res.status(500).json({ error: 'Error en el servidor al eliminar serie' })
     }
 })
 
