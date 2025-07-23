6da430101f669c812194eecd1486fddccd9f00f7
tincho <mvigil@fi.uba.ar>
2025-07-23 01:02:50 -0300

    elimina ñs

--- backend/rutas/usuarios.js
+++ backend/rutas/usuarios.js
@@ -84,14 +84,14 @@ router.delete('/:id', async (req, res) => {
 })
 
 router.post("/login", async (req, res) => {
-    const { nombre_usuario, contraseña } = req.body;
+    const { nombre_usuario, contrasenia } = req.body;
 
     try {
         const usuario = await usuarioModelo.obtenerPorNombreUsuario(nombre_usuario);
         if (!usuario) {
             return res.status(404).json({ error: 'El nombre de usuario no existe' });
         }
-        if (usuario.contrasenia === contraseña) {
+        if (usuario.contrasenia === contrasenia) {
             res.status(200).json({ mensaje: 'Usuario logeado' });
         } else {
             res.status(401).json({ mensaje: 'La contraseña no coincide' });
